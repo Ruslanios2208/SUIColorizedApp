8db099ce5130ec158c247335ff2e58ae4cf06ea1
Ruslanios22 <ilgamovich@gmail.com>
2022-12-14 01:29:10 +0300

    Without implementation textFields

--- SUIColorizedApp/ContentView.swift
+++ SUIColorizedApp/ContentView.swift
@@ -8,19 +8,100 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var redSliderValue = Double.random(in: 0...255)
+    @State private var greenSliderValue = Double.random(in: 0...255)
+    @State private var blueSliderValue = Double.random(in: 0...255)
+    
+    @State private var redTextFieldValue = ""
+    @State private var greenTextFieldValue = ""
+    @State private var blueTextFieldValue = ""
+    
+    @State private var alertPresented = false
+            
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(.systemIndigo)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 30) {
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundColor(
+                        Color(
+                            red: redSliderValue / 255,
+                            green: greenSliderValue / 255,
+                            blue: blueSliderValue / 255
+                        )
+                    )
+                    .frame(height: 170)
+                    .overlay(RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.white, lineWidth: 2))
+                
+                VStack {
+                    ColorSliderView(
+                        value: $redSliderValue,
+                        text: $redTextFieldValue,
+                        minTrackColor: .red
+                    )
+                    ColorSliderView(
+                        value: $greenSliderValue,
+                        text: $greenTextFieldValue,
+                        minTrackColor: .green
+                    )
+                    ColorSliderView(
+                        value: $blueSliderValue,
+                        text: $blueTextFieldValue,
+                        minTrackColor: .blue
+                    )
+                }
+                Spacer()
+            }
+            .padding()
+            .toolbar {
+                ToolbarItem(placement: .keyboard) {
+                    Button("Done", action: checkValue)
+                        .alert("Wrong format", isPresented: $alertPresented, actions: {}) {
+                            Text("Enter the number from 0 to 255")
+                        }
+                }
+            }
         }
-        .padding()
+    }
+    
+    private func checkValue() {
+        if let number = Double(redTextFieldValue) {
+            if number >= 0, number <= 255 {
+                redSliderValue = number
+                return
+            }
+        }
+        redTextFieldValue = ""
+        alertPresented.toggle()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ColorSliderView: View {
+    @Binding var value: Double
+    @Binding var text: String
+    let minTrackColor: Color
+    
+    var body: some View {
+        HStack {
+            Text("\(lround(value))")
+                .foregroundColor(.white)
+                .frame(width: 35)
+            Slider(value: $value, in: 0...255, step: 1)
+                .accentColor(minTrackColor)
+            TextField("\(lround(value))", text: $text)
+                .textFieldStyle(.roundedBorder)
+                .frame(width: 50)
+                .keyboardType(.numberPad)
+        }
     }
 }
