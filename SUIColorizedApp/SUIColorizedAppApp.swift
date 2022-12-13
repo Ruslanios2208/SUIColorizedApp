77770d1d7a7227c7134903d2497e0dde782f90d4
Ruslanios22 <ilgamovich@gmail.com>
2022-12-13 19:01:53 +0300

    Initial Commit

---
+++ SUIColorizedApp/SUIColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIColorizedAppApp.swift
+//  SUIColorizedApp
+//
+//  Created by Руслан Шигапов on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SUIColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
